0247f4f96fb1af0a1680d2e5dd45b158d9356c75
TheEUF <kinga@cubeipo.asia>
2025-06-03 14:23:55 +0800

    Add simple multi-user support

---
+++ TodoMvc/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using TodoMvc.Data;
+
+namespace TodoMvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public AccountController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == username && u.Password == password);
+            if (user != null)
+            {
+                HttpContext.Session.SetInt32("UserId", user.Id);
+                HttpContext.Session.SetString("Username", user.Username);
+                return RedirectToAction("Index", "Todo");
+            }
+            ViewBag.Error = "Invalid credentials";
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
