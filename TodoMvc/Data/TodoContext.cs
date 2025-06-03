0939cd25bc5832c2f7656dec5c6fd447bef1db7d
Gilbert <yungkeng@gmail.com>
2025-06-03 15:29:57 +0800

    fixed bug for user login

--- TodoMvc/Data/TodoContext.cs
+++ TodoMvc/Data/TodoContext.cs
@@ -20,9 +20,9 @@ namespace TodoMvc.Data
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<User>().HasData(
-                new User { Id = 1, Username = "alice", Password = "123456" },
-                new User { Id = 2, Username = "bob", Password = "123456" },
-                new User { Id = 3, Username = "charlie", Password = "123456" }
+                new User { Id = 1, Username = "A01", Password = "123456" },
+                new User { Id = 2, Username = "A02", Password = "123456" },
+                new User { Id = 3, Username = "A03", Password = "123456" }
             );
         }
 
